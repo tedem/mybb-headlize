d4194258eeb3760a11512a719a2add0712575169
Medet Erdal <hello@tedem.dev>
2025-11-05 22:26:57 +0300

    refactor: update Rector configuration

--- rector.php
+++ rector.php
@@ -3,17 +3,22 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__.'/inc/plugins',
+        __DIR__ . '/inc/plugins/headlize.php',
     ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
-        strictBooleans: true,
-    )
-    ->withPhpSets(php80: true);
+    ->withSets([
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+    ])
+    ->withSkip([
+        RemoveUnusedVariableAssignRector::class,
+    ])
+    ->withPhpVersion(PhpVersion::PHP_74);
